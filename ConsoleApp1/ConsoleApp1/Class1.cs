695bdeffb5e2132c56c657738d6fffcd4931b2c7
abdalrahmanalhalaq <121585453+abdalrahmanalhalaq@users.noreply.github.com>
2023-03-11 18:27:40 +0200

    last edit c#

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Request
+    {
+  
+
+
+
+        string id = "1";
+        string url = "http://127.0.0.1";
+        string headers = "autorization";
+       Console.Write(id ,url,headers);
+    }
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+class father
+{
+    string name = "ahmad";
+    string phone = "606949939";
+}
